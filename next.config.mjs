63d4301ea30ace7345f35f8019807a24e6969370
HaroonAzizi <haroonazizi15@gmail.com>
2025-02-11 00:57:28 +0430

    this should work fine with the domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: "/",
+};
 
 export default nextConfig;
