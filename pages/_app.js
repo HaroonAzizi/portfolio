12da840f43986fb8ff89cf1a7e77eff01367c599
HaroonAzizi <haroonazizi15@gmail.com>
2025-02-12 17:29:03 +0430

    added og-image

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,20 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        
+        {/* Open Graph meta tags */}
+        <meta property="og:title" content="Haroon Azizi - Software Engineer" />
+        <meta property="og:description" content="Personal portfolio and blog of Haroon Azizi, a software engineer specializing in web and mobile development." />
+        <meta property="og:image" content="https://haroonazizi.com/images/og-image.jpg" />
+        <meta property="og:url" content="https://haroonazizi.com" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Card meta tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@az_haroon" />
+        <meta name="twitter:title" content="Haroon Azizi - Software Engineer" />
+        <meta name="twitter:description" content="Personal portfolio and blog of Haroon Azizi, a software engineer specializing in web and mobile development." />
+        <meta name="twitter:image" content="https://haroonazizi.com/images/og-image.jpg" />
       </Head>
       <Layout>
         <Component {...pageProps} />
